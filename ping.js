628b40b61f31e3093829c102ed3396f4454e53ea
lalit.tanwar <lalit.tanwar07@gmail.com>
2025-02-06 16:13:24 +0530

    updated deps

--- ping.js
+++ ping.js
@@ -2,10 +2,14 @@
 
 const mongon = require('./index');
 const MongonSchema = require('./mongon_schema');
+const {timely} = require('@bootloader/utils');
+
 module.exports = {
     async push(){
         let MongonModel = mongon.model(MongonSchema);
-        let MongonDoc = new MongonModel({});
+        let MongonDoc = new MongonModel({
+            createdAt : timely.getTimeIndex()
+        });
         return MongonDoc.save().then((result)=>{
             return MongonModel.findById(result._id).exec().then((docs)=>{
                 return docs;
